ac5831f4886a5810cc5ec3aec75f959b3314f414
Dan Benston <dbenston07@gmail.com>
2025-08-13 19:44:05 -0500

    Added Discordant Stars factions, planets, and systems to the initTableData file. Added image files for new systems.

--- core/models/constants/anomalyConstants.py
+++ core/models/constants/anomalyConstants.py
@@ -7,4 +7,8 @@ class AnomalyType(models.TextChoices):
     MUATT_SUPERNOVA = "muatt-supernova", "Muatt Supernova"
     NEBULA = "Nebula", "Nebula"
     SUPERNOVA = "supernova", "Supernova"
+    ZELIAN_ASTEROID_FIELD = "zelian-asteroid-field", "Zelian Asteroid Field"
+    MYKO_CATACLYSM = "myko-cataclysm", "Myko Cataclysm"
+    ASTEROID_NEBULA = "asteroid-nebula", "Asteroid Nebula"
+    ASTEROID_RIFT = "asteroid-rift", "Asteroid Rift"
     NONE = "none", "None"
